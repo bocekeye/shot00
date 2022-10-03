76ae43740893883caed02ad4ec824841f0061794
bocekeye <yokoshou3236@icloud.com>
2022-10-03 11:21:09 +0900

    弾のバリエーション追加のための準備の作成

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -11,7 +11,6 @@ SceneMain::SceneMain()
 {
 	m_hPlayerGraphic = -1;
 	m_hShotGraphic = -1;
-	m_shotInterval = 0;
 }
 SceneMain::~SceneMain()
 {
@@ -26,12 +25,13 @@ void SceneMain::init()
 
 	m_player.setHandle(m_hPlayerGraphic);
 	m_player.init();
+	m_player.setMain(this);
 
 	for (auto& shot : m_shot)
 	{
 		shot.setHandle(m_hShotGraphic);
 	}
-	m_shotInterval = 0;
+
 }
 
 // èIóπèàóù
@@ -48,25 +48,7 @@ void SceneMain::update()
 	for (auto& shot : m_shot)
 	{
 		shot.update();
-	}
 
-	m_shotInterval--;
-	if (m_shotInterval < 0) m_shotInterval = 0;
-	
-
-	// ÉLÅ[ì¸óÕèàóù
-	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-	if( (padState & PAD_INPUT_1) && (m_shotInterval <= 0))
-	{
-		for (auto& shot : m_shot)
-		{
-			if (shot.isExist()) continue;
-
-			shot.start(m_player.getPos());
-			m_shotInterval = kShotInterval;
-			break;
-			
-		}
 	}
 }
 
@@ -86,5 +68,17 @@ void SceneMain::draw()
 	{
 		shot.draw(); shotNum++;
 	}
-	DrawFormatString(0, 0, GetColor(255, 255, 255), "íeÇÃêî : %d", shotNum);
+	DrawFormatString(0, 0, GetColor(255, 255, 255), "íeÇÃêî :%d", shotNum);
+}
+
+bool SceneMain::createShot(Vec2 pos)
+{
+	for (auto& shot : m_shot)
+	{
+		if (shot.isExist()) continue;
+
+		shot.start(m_player.getPos());
+		return true;
+	}
+	return false;
 }
